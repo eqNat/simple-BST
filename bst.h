c3075d10fb32ff1521f2a2ec2e9dd52a205f95b1
Nuclear-Catapult <nuclear.catapult@gmail.com>
2019-12-15 14:46:18 -0600

    removed unnecessary libraries

--- bst.h
+++ bst.h
@@ -1,11 +1,9 @@
 #pragma once
 
-#include <stdint.h>
-
 struct BST_Node;
 
-void insert(struct BST_Node** node, int32_t data);
-int insertUnique(struct BST_Node** node, int32_t data);
+void insert(struct BST_Node** node, int data);
+int insertUnique(struct BST_Node** node, int data);
 
 void traversePrint(struct BST_Node* iter);
 
