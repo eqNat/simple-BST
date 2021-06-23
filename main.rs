a90ab9e8fae293fbafa75984d0667ae4a19e7519
Nuclear-Catapult <nuclear.catapult@gmail.com>
2021-06-23 04:02:48 -0500

    BST now works

--- main.rs
+++ main.rs
@@ -1,16 +1,21 @@
 struct Tree(Box<Option<(i32, Tree, Tree)>>);
 
-fn allocate(x: &mut Tree) {
-    println!("x data: {:p}", x.0);
-
-    if let None = *x.0 {
-        *x = Tree(Box::new(Some((35, Tree(Box::new(None)), Tree(Box::new(None))))));
+fn insert(mut x: &mut Tree, val: i32) {
+    while let Some((v, ref mut left, ref mut right)) = *x.0 {
+        x =  if v > val {println!("left"); left } else {println!("right"); right };
     }
+    *x = Tree(Box::new(Some((val, Tree(Box::new(None)), Tree(Box::new(None))))));
+    println!("inserted");
 }
 
 fn main() {
     let mut b = Tree(Box::new(None));
-    allocate(&mut b);
+    insert(&mut b, 100);
+    insert(&mut b, 50);
+    insert(&mut b, 25);
+    insert(&mut b, 150);
+    insert(&mut b, 125);
+    insert(&mut b, 175);
     if let Some((i, _, _)) = *b.0 {
         println!("root data: {}", i);
     }
