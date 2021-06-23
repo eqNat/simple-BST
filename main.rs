0e78b3d44f6188eece785b3df61d5b06ce7d9af8
Nuclear-Catapult <nuclear.catapult@gmail.com>
2021-06-23 04:30:42 -0500

    Tree only accepts unique values

--- main.rs
+++ main.rs
@@ -1,21 +1,25 @@
 struct Tree(Box<Option<(i32, Tree, Tree)>>);
 
-fn insert(mut x: &mut Tree, val: i32) {
-    while let Some((v, ref mut left, ref mut right)) = *x.0 {
-        x =  if v > val {println!("left"); left } else {println!("right"); right };
+impl Tree {
+    fn insert(mut self: &mut Tree, val: i32) {
+        while let Some((v, ref mut left, ref mut right)) = *self.0 {
+            if v == val {
+                return
+            }
+            self =  if v > val { left } else { right };
+        }
+        *self = Tree(Box::new(Some((val, Tree(Box::new(None)), Tree(Box::new(None))))));
     }
-    *x = Tree(Box::new(Some((val, Tree(Box::new(None)), Tree(Box::new(None))))));
-    println!("inserted");
 }
 
 fn main() {
     let mut b = Tree(Box::new(None));
-    insert(&mut b, 100);
-    insert(&mut b, 50);
-    insert(&mut b, 25);
-    insert(&mut b, 150);
-    insert(&mut b, 125);
-    insert(&mut b, 175);
+    b.insert(100);
+    b.insert(50);
+    b.insert(25);
+    b.insert(150);
+    b.insert(125);
+    b.insert(175);
     if let Some((i, _, _)) = *b.0 {
         println!("root data: {}", i);
     }
