3da527c22fcec5533356a25a994f03e0b6770950
Nuclear-Catapult <nuclear.catapult@gmail.com>
2021-06-23 05:55:01 -0500

    Tree::new()

--- main.rs
+++ main.rs
@@ -7,21 +7,26 @@ struct Tree<T>(Box<Option<(T, Tree<T>, Tree<T>)>>);
 
 impl<T: std::cmp::PartialEq + std::cmp::PartialOrd> Tree<T>
 {
+    fn new() -> Tree<T>
+    {
+        Tree(Box::new(None))
+    }
+
     fn insert(mut self: &mut Tree<T>, val: T) -> Status
     {
-        while let Some((ref v, ref mut left, ref mut right)) = *self.0
+        while let Some((ref data, ref mut left, ref mut right)) = *self.0
         {
-            if *v == val { return Status::Duplicate }
-            self = if *v > val { left } else { right }
+            if *data == val { return Status::Duplicate }
+            self = if *data > val { left } else { right }
         }
-        *self = Tree(Box::new(Some((val, Tree(Box::new(None)), Tree(Box::new(None))))));
+        *self = Tree(Box::new(Some((val, Tree::new(), Tree::new()))));
         return Status::Success
     }
 }
 
 fn main()
 {
-    let mut b = Tree(Box::new(None));
+    let mut b = Tree::new();
     b.insert(100);
     b.insert(50);
     b.insert(25);
