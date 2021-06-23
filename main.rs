f889ee6516430ee5e2719db8f4455cfde38d0e15
Nuclear-Catapult <nuclear.catapult@gmail.com>
2021-06-23 06:20:38 -0500

    Tree::print()

--- main.rs
+++ main.rs
@@ -5,7 +5,7 @@ enum Status {
 
 struct Tree<T>(Box<Option<(T, Tree<T>, Tree<T>)>>);
 
-impl<T: std::cmp::PartialEq + std::cmp::PartialOrd> Tree<T>
+impl<T: std::cmp::PartialEq + std::cmp::PartialOrd + std::fmt::Display> Tree<T>
 {
     fn new() -> Tree<T>
     {
@@ -22,6 +22,16 @@ impl<T: std::cmp::PartialEq + std::cmp::PartialOrd> Tree<T>
         *self = Tree(Box::new(Some((val, Tree::new(), Tree::new()))));
         return Status::Success
     }
+
+    fn print(self)
+    {
+        if let Some((data, left, right)) = *self.0
+        {
+            Tree::print(left);
+            println!("{}", data);
+            Tree::print(right);
+        }
+    }
 }
 
 fn main()
@@ -33,7 +43,6 @@ fn main()
     b.insert(150);
     b.insert(125);
     b.insert(175);
-    if let Some((i, _, _)) = *b.0 {
-        println!("root data: {}", i);
-    }
+
+    b.print();
 }
