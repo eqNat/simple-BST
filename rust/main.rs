36e4f7bf56ccb74a9d95a83145dce8dca3797f5e
0x2bad <nuclear.catapult@gmail.com>
2021-12-29 18:54:44 -0600

    changed 'Tree' from struct to enum

---
+++ rust/main.rs
@@ -0,0 +1,74 @@
+enum Status {
+    Unique,
+    Duplicate,
+}
+
+enum Tree {
+    Node(i32, Box<Tree>, Box<Tree>),
+    Nil,
+}
+
+impl Tree
+{
+    fn new() -> Tree
+    {
+        Tree::Nil
+    }
+
+    fn insert(mut self: &mut Tree, val: i32) -> Status
+    {
+        while let Tree::Node(data, ref mut left, ref mut right) = *self
+        {
+            if data == val { return Status::Duplicate }
+            self = if data > val { left } else { right }
+        }
+        *self = Tree::Node(val, Box::new(Tree::Nil), Box::new(Tree::Nil));
+        return Status::Unique
+    }
+
+    fn find(mut self: &mut Tree, val: i32) -> Status
+    {
+        while let Tree::Node(data, ref mut left, ref mut right) = *self
+        {
+            if data == val { return Status::Duplicate; }
+            self = if data > val { left } else { right }
+        }
+        return Status::Unique
+    }
+
+    fn print(self)
+    {
+        if let Tree::Node(data, left, right) = self
+        {
+            Tree::print(*left);
+            println!("{}", data);
+            Tree::print(*right);
+        }
+    }
+}
+
+fn main()
+{
+    let mut b = Tree::new();
+    b.insert(100);
+    b.insert(50);
+    b.insert(25);
+    b.insert(150);
+    b.insert(125);
+    b.insert(175);
+    b.insert(75);
+
+    if let Status::Duplicate = b.find(125) {
+        println!("125 is found");
+    } else {
+        println!("125 is not found");
+    }
+
+    if let Status::Duplicate = b.find(127) {
+        println!("127 is found");
+    } else {
+        println!("127 is not found");
+    }
+
+    b.print();
+}
